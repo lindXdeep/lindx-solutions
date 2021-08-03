512c90ae5e7ba5f4dbf629b509315e6c2b5dfa10
lindx <linder.igor@gmail.com>
2021-08-04 00:16:41 +0300

    010-Цель установлена

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 /**
  * App
  */
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("init javarush");
+    
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(
+      (a > 0 & b > 0) ? 1 :
+      (a<0 & b> 0)? 2 :
+      (a < 0 & b < 0) ? 3 : 4
+    );
   }
 }
