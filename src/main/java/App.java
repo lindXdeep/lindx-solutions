f71c66c9cdb7b254788557b34fecc1704174e712
[IgorLinder] <linder.igor@gmail.com>
2021-08-03 22:49:47 +0300

    004-Минимум двух чисел (#4)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,14 @@
-/**
- * App
- */
+import java.util.Scanner;
+
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("init javarush");
+
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println((a > b) ? b : a);
   }
 }
