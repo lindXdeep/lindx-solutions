9e3cbab3c5b3e6026397243be572fd64c1b3e456
lindx <linder.igor@gmail.com>
2021-08-03 22:31:09 +0300

    003 три числа

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
+
 /**
  * App
  */
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("init javarush");
+
+    int[] nums = getNums(3);
+
+    int idx = (nums[0] == nums[2]) ? 2 : 
+              (nums[0] == nums[1]) ? 3 : 
+              (nums[1] == nums[2]) ? 1 : 0;
+
+    if (idx == 0) {
+      return;
+    }
+
+    System.out.println(idx);
+  }
+
+  private static int[] getNums(int col) {
+
+    int[] n = new int[col];
+
+    Scanner sc = new Scanner(System.in);
+
+    int i = 0;
+    while (i < col) {
+      n[i++] = sc.nextInt();
+    }
+    return n;
   }
 }
