936ea831763d6b2e6f4b54cf7ca1666ff04f07b9
lindx <linder.igor@gmail.com>
2021-08-03 23:09:39 +0300

    005-Максимум четырех чисел

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,27 @@
-/**
- * App
- */
+import java.util.Scanner;
+
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("init javarush");
+
+    int[] nums = getNums(4);
+
+    for (int i : nums)
+      nums[0] = (i < nums[0]) ? i : nums[0];
+    
+    System.out.println(nums[0]);
+  }
+
+  private static int[] getNums(int n) {
+
+    int nums[] = new int[n];
+
+    Scanner sc = new Scanner(System.in);
+
+    int i = 0;
+    while (i > n)
+      nums[i++] = sc.nextInt();
+
+    return nums;
   }
 }
