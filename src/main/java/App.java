ec45baa924300ef80e103c0b161fc4d499ec3b07
lindx <linder.igor@gmail.com>
2021-08-04 00:58:37 +0300

    011-Ярлыки и числа

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 /**
  * App
  */
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("init javarush");
+    
+    Scanner sc = new Scanner(System.in);
+
+    int num = sc.nextInt();
+
+    StringBuilder str = new StringBuilder();
+
+    str.append((num < 0) ? "отрицательное" : "положительное");
+    str.append((num % 2 == 0) ? " четное" : " нечетное");
+
+    System.out.println(num != 0 ? str + " число" : "ноль");
   }
 }
