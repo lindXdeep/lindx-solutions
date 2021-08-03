cd084d630f67da30df44befed56507e15c2e5357
lindx <linder.igor@gmail.com>
2021-08-03 23:48:39 +0300

    007-Настя или Настя?

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 /**
  * App
  */
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("init javarush");
+
+    Scanner sc = new Scanner(System.in);
+
+    String str1 = sc.next();
+    String str2 = sc.next();
+    String result;
+
+    if (str1.equals(str2)) {
+      result = "Имена идентичны";
+    } else if (str1.length() == str2.length()) {
+      result = "Длины имен равны";
+    } else {
+      return;
+    }
+
+    System.out.println(result);
   }
 }
