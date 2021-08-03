46c162dc32747672748f8ea0c81212ba53b0104d
lindx <linder.igor@gmail.com>
2021-08-03 23:55:26 +0300

    18+

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * App
  */
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("init javarush");
+
+    Scanner sc = new Scanner(System.in);
+
+    String name = sc.next();
+    int age = sc.nextInt();
+
+    if (age < 18) {
+      System.out.println("Подрасти еще");
+    }
+
   }
 }
