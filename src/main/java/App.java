2a84caad2ffcc66ef71671bc55949738fbe1ae19
lindx <linder.igor@gmail.com>
2021-08-04 00:23:07 +0300

    solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,16 +6,6 @@ import java.util.Scanner;
 public class App {
 
   public static void main(String[] args) {
-    
-    Scanner sc = new Scanner(System.in);
-
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-
-    System.out.println(
-      (a > 0 & b > 0) ? 1 :
-      (a<0 & b> 0)? 2 :
-      (a < 0 & b < 0) ? 3 : 4
-    );
+   
   }
 }
