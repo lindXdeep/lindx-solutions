e6f0ce8fdf1ffc7e963aa3bf47e4993454db291f
lindx <linder.igor@gmail.com>
2021-08-04 03:02:53 +0300

    Описываем числа

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 /**
  * App
  */
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("init javarush");
+    
+    Scanner sc = new Scanner(System.in);
+
+    StringBuilder build = new StringBuilder();
+
+    int n = sc.nextInt();
+
+    if (n > 0 & n < 1000) {
+
+      int l = String.valueOf(n).length();
+
+      build.append((n % 2 == 0) ? "четное " : "нечетное ");
+      build.append(l == 1 ? "однозначное" : l == 2 ? "двузначное" : "трехзначное");
+      System.out.println(build.toString() + " число");
+    }
   }
 }
